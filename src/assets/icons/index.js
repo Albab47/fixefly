72e5b4d513489fd249d95f54c40e5a56819f4856
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-11-26 23:48:42 +0600

    Add svg images and icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,15 @@
+import homeIcon from "./home.svg";
+import aboutUsIcon from "./about-us.svg";
+import servicesIcon from "./services.svg";
+import blogIcon from "./blog.svg";
+import caseStudyIcon from "./case-study.svg";
+import rightArrowIcon from "./arrow-right.svg";
+
+export {
+  homeIcon,
+  aboutUsIcon,
+  servicesIcon,
+  blogIcon,
+  caseStudyIcon,
+  rightArrowIcon,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+import {aboutUsIcon, blogIcon, caseStudyIcon, homeIcon, servicesIcon} from '../assets/icons'
+
+export const navLinks = [
+    {address: "#", label: "Home", icon: homeIcon},
+    {address: "#about-us", label: "About Us", icon: aboutUsIcon},
+    {address: "#services", label: "Services", icon: servicesIcon},
+    {address: "#blog", label: "Blog", icon: blogIcon},
+    {address: "#case-study", label: "Case Study", icon: caseStudyIcon},
+]
