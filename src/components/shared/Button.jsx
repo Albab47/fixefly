c7eaf16a79444498ac707af8cf05bdd81f708874
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-11-28 17:04:20 +0600

    Update button padding

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({ children, hoverTxt, icon: Icon, outline = false }) => {
   return (
     <button
-      className={`group relative text-lg font-semibold flex items-center justify-center h-full gap-3 px-5 py-1 ${
+      className={`group relative text-lg font-semibold flex items-center justify-center h-full gap-3 px-6 py-5 ${
         outline ? "bg-white text-black" : "bg-primary text-white"
       } rounded-full transition-transform duration-300 ease-in-out`}
     >
@@ -11,13 +11,12 @@ const Button = ({ children, hoverTxt, icon: Icon, outline = false }) => {
           <span className="hidden transition-all duration-500 group-hover:block">{hoverTxt}</span>
         </>
       ) : (
-        <span>{children}</span>
+        <span className="flex items-center justify-center text-center text-lg gap-3">{children}</span>
       )}
       {Icon && (
         <span className="relative group-hover:hidden only:-mx-6">
           <Icon className="size-5" />
         </span>
-        
       )}
     </button>
   );
